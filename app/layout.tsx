407e80e2bc5ca324fe64e169e398824931eff564
Chapiuskys <gabrielmp2709@gmail.com>
2026-07-17 22:03:57 -0300

    feat: add initial header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <h1>layout</h1>
-      {children}
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
 }
